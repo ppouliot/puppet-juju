2b7ee4a35bea7d870a11d4917d7856c7b0bd092c
primeministerp <primeministerpete@gmail.com>
2015-05-12 13:32:48 -0400

    adding a user to run juju commands

--- manifests/generic_config.pp
+++ manifests/generic_config.pp
@@ -1,10 +1,16 @@
-# == Class: juju::generic_config
+# == Define: juju::generic_config
 #
-class juju::generic_config {
+define juju::generic_config (){
+  case $name {
+    'root':{ $basedir = undef }
+    default:{ $basedir = '/home' }
+  }
   exec {'juju_generate_generic_config':
     command => '/usr/bin/juju generate-config',
-    user    => 'root',
-    cwd     => '/root',
-    creates => '/root/.juju/environments.yaml'
+    user    => $name,
+    cwd     => "${basedir}/${name}",
+    creates => ['${basedir}/${name}/.juju/',
+                '${basedir}/${name}/.juju/environments.yaml'
+                '${basedir}/${name}/.juju/ssh'],
   }
 }
---
+++ manifests/user.pp
@@ -0,0 +1,23 @@
+# == Define: juju::user
+#
+define juju::user {
+  user { '${name}':
+    ensure           => 'present',
+    comment          => '${name}',
+    gid              => '0',
+    home             => '/home/${name}',
+    password         => '${name}',
+    password_max_age => '99999',
+    password_min_age => '0',
+    shell            => '/bin/bash',
+    uid              => '0',
+  } ->
+
+  juju::generic_config{$name:} ->
+
+  file {["/home/${name}/.juju",
+         "/home/${name}/.juju/ssh",
+    ensure => directory,
+  }
+
+}
