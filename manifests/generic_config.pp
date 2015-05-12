774167a7825df8136cc3db774616c13e09713f81
primeministerp <primeministerpete@gmail.com>
2015-05-12 13:52:38 -0400

    adding variable

--- manifests/generic_config.pp
+++ manifests/generic_config.pp
@@ -1,16 +1,16 @@
 # == Define: juju::generic_config
 #
-define juju::generic_config ($juju_username = $name){
-  case $juju_username {
-    'root':{ $basedir = "/${juju_username}/" }
-    default:{ $basedir = "/home/${juju_username}/" }
+define juju::generic_config (){
+  case $name {
+    'root':{ $basedir = "/${name}/" }
+    default:{ $basedir = "/home/${name}/" }
   }
   exec {'juju_generate_generic_config':
     command => '/usr/bin/juju generate-config',
     user    => $name,
     cwd     => $basedir,
-    creates => ['${basedir}/.juju/',
-                '${basedir}/.juju/environments.yaml',
-                '${basedir}/.juju/ssh'],
+    creates => ["${basedir}/.juju/",
+                "${basedir}/.juju/environments.yaml",
+                "${basedir}/.juju/ssh"],
   }
 }
