107623cb9f3befe984aec388b14b6f74b84195c8
primeministerp <primeministerpete@gmail.com>
2015-08-23 08:51:28 -0400

    integrating environments

--- manifests/default_user.pp
+++ manifests/default_user.pp
@@ -34,30 +34,30 @@ class juju::default_user (
   }
 
 
-#  juju::generic_config{'juju':
-#    require => User['juju'],
-#  } ->
-#  file{"/home/juju/.juju":
-#    ensure  => directory,
-#    require => User['juju'],
-#  } ->
-#  file{"/home/juju/.juju/ssh":
-#    ensure  => directory,
-#    require => User['juju'],
-#  } ->
-#  file{[ "/home/juju/.juju/ssh/juju_id_rsa",
-#         "/home/juju/.juju/ssh/juju_id_rsa.pub"]:
-#    ensure  => file,
-#    require => User['juju'],
-#  }
+  file{"/home/juju/.juju":
+    ensure  => directory,
+    require => [User['juju'], Exec['juju_default_user-generate_generic_config']],
+  } ->
+  file{"/home/juju/.juju/ssh":
+    ensure  => directory,
+    require => [User['juju'], Exec['juju_default_user-generate_generic_config']],
+  } ->
+  file{[ "/home/juju/.juju/ssh/juju_id_rsa",
+         "/home/juju/.juju/ssh/juju_id_rsa.pub"]:
+    ensure  => file,
+    require => [User['juju'], Exec['juju_default_user-generate_generic_config']],
+  }
 
 
-#  concat { "/home/juju/.juju/environment.yaml":
-#    mode    => 0644,
-#  }
-#  concat::fragment {"juju.environtment.yaml_header":
-#    target  => "/home/juju/.juju/environment.yaml",
-#    content => template("juju/environments.header.erb"),
-#    order   => 01,
-#  }
+  concat { "/home/juju/.juju/environment.yaml":
+    mode    => 0644,
+    require => [User['juju'], Exec['juju_default_user-generate_generic_config']],
+  }
+  concat::fragment {"juju.environtment.yaml_header":
+    target  => "/home/juju/.juju/environment.yaml",
+    content => template("juju/environments.header.erb"),
+    order   => 01,
+    require => [User['juju'], Exec['juju_default_user-generate_generic_config']],
+  }
+
 }
