187935796f490a2ec75dafa679c3439d631722f7
primeministerp <primeministerpete@gmail.com>
2015-08-23 08:02:57 -0400

    fixing generic_config and default_user

--- manifests/generic_config.pp
+++ manifests/generic_config.pp
@@ -9,15 +9,19 @@ define juju::generic_config () {
       $juju_home = "/home/${name}/"
     }
   }
-  exec {"juju_generate_generic_config-$name":
-    command => '/usr/bin/juju generate-config',
-    user    => $name,
-    cwd     => $juju_home,
-    creates => ["${juju_home}/.juju/",
-                "${juju_home}/.juju/environments.yaml",
-                "${juju_home}/.juju/ssh",
-                "${juju_home}/.juju/ssh/juju_id_rsa",
-                "${juju_home}/.juju/ssh/juju_id_rsa.pub"],
-    onlyif    => "/usr/bin/test ! -f ${juju_home}/.juju",
+  exec{"juju_user-$name-generate_generic_config":
+    command     => '/usr/bin/juju generate-config',
+    environment => ["JUJU_HOME=${juju_home}"],
+    cwd         => '${juju_home}',
+    user        => $name,
+    creates     => ["${juju_home}/.juju/",
+                    "${juju_home}/.juju/environments.yaml",
+                    "${juju_home}/.juju/ssh",
+                    "${juju_home}/.juju/ssh/juju_id_rsa",
+                    "${juju_home}/.juju/ssh/juju_id_rsa.pub"],
+    onlyif      => "/usr/bin/test ! -f ${juju_home}/.juju",
+    require     => User[$name],
+    logoutput   => true,
   }
+
 }
