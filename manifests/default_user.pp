0179689c0d2d3d6520159e718a118cc7d1f88dfa
peter pouliot <ppouliot@microsoft.com>
2016-03-08 19:17:00 +0000

    lint test cleanup

--- manifests/default_user.pp
+++ manifests/default_user.pp
@@ -23,39 +23,39 @@ class juju::default_user (
     environment => ['JUJU_HOME=/home/juju/.juju'],
     cwd         => '/home/juju',
     user        => 'juju',
-    creates     => ["/home/juju/.juju/",
-                    "/home/juju/.juju/environments.yaml",
-                    "/home/juju/.juju/ssh",
-                    "/home/juju/.juju/ssh/juju_id_rsa",
-                    "/home/juju/.juju/ssh/juju_id_rsa.pub"],
-    onlyif      => "/usr/bin/test ! -f /home/juju/.juju",
+    creates     => ['/home/juju/.juju/',
+                    '/home/juju/.juju/environments.yaml',
+                    '/home/juju/.juju/ssh',
+                    '/home/juju/.juju/ssh/juju_id_rsa',
+                    '/home/juju/.juju/ssh/juju_id_rsa.pub'],
+    onlyif      => '/usr/bin/test ! -f /home/juju/.juju',
     require     => User['juju'],
     logoutput   => true,
   } ->
 
 
-  file{"/home/juju/.juju":
+  file{'/home/juju/.juju':
     ensure  => directory,
     require => [User['juju'], Exec['juju_default_user-generate_generic_config']],
   } ->
-  file{"/home/juju/.juju/ssh":
+  file{'/home/juju/.juju/ssh':
     ensure  => directory,
     require => [User['juju'], Exec['juju_default_user-generate_generic_config']],
   } ->
-  file{[ "/home/juju/.juju/ssh/juju_id_rsa",
-         "/home/juju/.juju/ssh/juju_id_rsa.pub"]:
+  file{[ '/home/juju/.juju/ssh/juju_id_rsa',
+         '/home/juju/.juju/ssh/juju_id_rsa.pub']:
     ensure  => file,
     require => [User['juju'], Exec['juju_default_user-generate_generic_config']],
   }
 # Moving to Environment.pp for testing
 # ->
-#  concat { "/home/juju/.juju/environments.yaml":
+#  concat { '/home/juju/.juju/environments.yaml':
 #    mode    => 0644,
 #    require => [User['juju'], Exec['juju_default_user-generate_generic_config']],
 #  }
-#  concat::fragment {"juju.environtment.yaml_header":
-#    target  => "/home/juju/.juju/environments.yaml",
-#    content => template("juju/environments.header.erb"),
+#  concat::fragment {'juju.environtment.yaml_header':
+#    target  => '/home/juju/.juju/environments.yaml',
+#    content => template('juju/environments.header.erb'),
 #    order   => 01,
 #  }
 
--- manifests/juju_gui.pp
+++ manifests/juju_gui.pp
@@ -5,11 +5,11 @@ class juju::juju_gui(
 
 ) inherits juju::params {
   exec { 'juju-deploy-juju-gui-to-0':
-    command => '/usr/bin/juju deploy juju-gui --to 0',
+    command   => '/usr/bin/juju deploy juju-gui --to 0',
     user      => 'juju',
     pwd       => '/home/juju',
     timeout   => 0,
     logoutput => true,
     require   => [Package['juju'],User['juju']],
- }  
+  }
 }
